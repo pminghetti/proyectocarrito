761e0fc99a217bfffb2d7709ff760d4d40c5f434
pminghetti <otiminghetti@gmail.com>
2022-09-21 23:06:37 -0300

    subida del proyecto

---
+++ app.js
@@ -0,0 +1,171 @@
+// variables y constantes
+const cartContainer = document.querySelector('.cart-container');
+const productList = document.querySelector('.product-list');
+const cartList = document.querySelector('.cart-list');
+const cartTotalValue = document.getElementById('cart-total-value');
+const cartCountInfo = document.getElementById('cart-count-info');
+let cartItemID = 1;
+
+eventListeners();
+
+// eventos
+function eventListeners(){
+    window.addEventListener('DOMContentLoaded', () => {
+        loadJSON();
+        loadCart();
+    });
+    document.querySelector('.navbar-toggler').addEventListener('click', () => {
+        document.querySelector('.navbar-collapse').classList.toggle('show-navbar');
+    });
+
+    document.getElementById('cart-btn').addEventListener('click', () => {
+        cartContainer.classList.toggle('show-cart-container');
+    });
+
+    // agregar al carrito
+    productList.addEventListener('click', purchaseProduct);
+
+    // borrar del carrito
+    cartList.addEventListener('click', deleteProduct);
+}
+
+// actualizar carrito
+function updateCartInfo(){
+    let cartInfo = findCartInfo();
+    cartCountInfo.textContent = cartInfo.productCount;
+    cartTotalValue.textContent = cartInfo.total;
+}
+
+// carga de productosjson
+function loadJSON(){
+    fetch('tienda.json')
+    .then(response => response.json())
+    .then(data =>{
+        let html = '';
+        data.forEach(product => {
+            html += `
+                <div class = "product-item">
+                    <div class = "product-img">
+                        <img src = "${product.imgSrc}" alt = "product image">
+                        <button type = "button" class = "add-to-cart-btn">
+                            <i class = "fas fa-shopping-cart"></i>Agregar al Carrito
+                        </button>
+                    </div>
+
+                    <div class = "product-content">
+                        <h3 class = "product-name">${product.name}</h3>
+                        <span class = "product-category">${product.category}</span>
+                        <p class = "product-price">$${product.price}</p>
+                    </div>
+                </div>
+            `;
+        });
+        productList.innerHTML = html;
+    })
+    .catch(error => {
+        alert(`Usar live server`);
+    })
+}
+
+
+// seleccionar prod
+function purchaseProduct(e){
+    if(e.target.classList.contains('add-to-cart-btn')){
+        let product = e.target.parentElement.parentElement;
+        getProductInfo(product);
+    }
+}
+
+// inmfo produc en carrito
+function getProductInfo(product){
+    let productInfo = {
+        id: cartItemID,
+        imgSrc: product.querySelector('.product-img img').src,
+        name: product.querySelector('.product-name').textContent,
+        category: product.querySelector('.product-category').textContent,
+        price: product.querySelector('.product-price').textContent
+    }
+    cartItemID++;
+    addToCartList(productInfo);
+    saveProductInStorage(productInfo);
+}
+
+// agregar a
+function addToCartList(product){
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('cart-item');
+    cartItem.setAttribute('data-id', `${product.id}`);
+    cartItem.innerHTML = `
+        <img src = "${product.imgSrc}" alt = "product image">
+        <div class = "cart-item-info">
+            <h3 class = "cart-item-name">${product.name}</h3>
+            <span class = "cart-item-category">${product.category}</span>
+            <span class = "cart-item-price">${product.price}</span>
+        </div>
+
+        <button type = "button" class = "cart-item-del-btn">
+            <i class = "fas fa-times"></i>
+        </button>
+    `;
+    cartList.appendChild(cartItem);
+}
+
+// guardar en localstor
+function saveProductInStorage(item){
+    let products = getProductFromStorage();
+    products.push(item);
+    localStorage.setItem('products', JSON.stringify(products));
+    updateCartInfo();
+}
+
+
+function getProductFromStorage(){
+    return localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
+}
+
+//ç
+function loadCart(){
+    let products = getProductFromStorage();
+    if(products.length < 1){
+        cartItemID = 1; 
+    } else {
+        cartItemID = products[products.length - 1].id;
+        cartItemID++;
+    }
+    products.forEach(product => addToCartList(product));
+
+    updateCartInfo();
+}
+
+// calcular precio total
+function findCartInfo(){
+    let products = getProductFromStorage();
+    let total = products.reduce((acc, product) => {
+        let price = parseFloat(product.price.substr(1)); 
+        return acc += price;
+    }, 0); 
+
+    return{
+        total: total.toFixed(2),
+        productCount: products.length
+    }
+}
+
+//borar producto de carro y de locals
+function deleteProduct(e){
+    let cartItem;
+    if(e.target.tagName === "BUTTON"){
+        cartItem = e.target.parentElement;
+        cartItem.remove(); 
+    } else if(e.target.tagName === "I"){
+        cartItem = e.target.parentElement.parentElement;
+        cartItem.remove(); 
+    }
+
+    let products = getProductFromStorage();
+    let updatedProducts = products.filter(product => {
+        return product.id !== parseInt(cartItem.dataset.id);
+    });
+    localStorage.setItem('products', JSON.stringify(updatedProducts));
+    updateCartInfo();
+}
