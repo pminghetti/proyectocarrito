796723267f3dcdb8f607eed9276b6a2c1d4639e0
pminghetti <otiminghetti@gmail.com>
2022-10-05 22:36:06 -0300

    agregado fetch

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ function purchaseProduct(e){
     }
 }
 
-// inmfo produc en carrito
+// info produc en carrito
 function getProductInfo(product){
     let productInfo = {
         id: cartItemID,
@@ -172,3 +172,26 @@ function deleteProduct(e){
     localStorage.setItem('products', JSON.stringify(updatedProducts));
     updateCartInfo();
 }
+
+
+const listUsers = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+
+    let tableBody = ``;
+    users.forEach((user, index) => {
+        tableBody += `<tr>
+        <td class='centered'>${user.id}</td>
+        <td class='centered'>${user.name}</td>
+        <td class='centered'>${user.username}</td>
+        <td class='centered'>${user.email}</td>
+        <td class='centered'>${user.website}</td>
+        </tr>`;
+    });
+    // document.getElementById("tableBody_Users").innerHTML = tableBody;
+    tableBody_Users.innerHTML = tableBody;
+};
+
+window.addEventListener("load", function () {
+    listUsers();
+});
